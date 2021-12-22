b0b01288ef82b348306108ce88ab49f327d1f550
Orlandinho <horlandomagic@gmail.com>
2021-12-21 22:30:36 -0300

    Setting up Models, Controllers and Factories

---
+++ app/Models/Student.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'classroom_id',
+        'name',
+        'active',
+        'visitor',
+        'avatar'
+    ];
+
+    public function attendances()
+    {
+        return $this->hasMany(Attendance::class);
+    }
+}
